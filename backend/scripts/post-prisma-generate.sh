04d10c306784c0635a56907185024c9b2e48881e
SerhiiMaryniuk <mefissto.w@gmail.com>
2026-02-26 22:14:55 +0100

    fix: update post-prisma-generate script comments and package.json fields

--- backend/scripts/post-prisma-generate.sh
+++ backend/scripts/post-prisma-generate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is intended to be run after the Prisma client has been generated. 
+# It checks for the existence of a package.json file in the generated Prisma client directory and creates one if it doesn't exist. 
+# This is necessary for the generated Prisma client to be properly recognized as a package in CI/CD pipelines.
+
 dirname=$(dirname "$0") # Get the directory of the current script
 file_name="package.json"
 file_location=$dirname/../generated/prisma
@@ -13,9 +17,8 @@ if [ ! -f $file_path ]; then
   cat > $file_path << EOF
 {
   "name": "@generated/prisma",
-  "main": "client.js",
-  "types": "client.d.ts",
-  "type": "commonjs"
+  "main": "client.ts",
+  "types": "client.ts",
 }
 EOF
 else

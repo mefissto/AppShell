156e3d0dfa208a38408d5093393e48a697b99282
SerhiiMaryniuk <mefissto.w@gmail.com>
2026-02-26 22:25:22 +0100

    fix: standardize variable names in post-prisma-generate script for clarity

--- backend/scripts/post-prisma-generate.sh
+++ backend/scripts/post-prisma-generate.sh
@@ -5,23 +5,37 @@
 # This is necessary for the generated Prisma client to be properly recognized as a package in CI/CD pipelines.
 
 dirname=$(dirname "$0") # Get the directory of the current script
-file_name="package.json"
 file_location=$dirname/../generated/prisma
-file_path=$file_location/$file_name
+package_file_name="package.json"
+package_file_path=$file_location/$package_file_name
+  
 
-echo "Checking if file '$file_name' exists at '$file_location'..."
+echo "Checking if file '$package_file_name' exists at '$file_location'..."
 
-if [ ! -f $file_path ]; then
-  echo "File '$file_name' does not exist at '$file_location', creating it..."
-  touch $file_path
-  cat > $file_path << EOF
+if [ ! -f $package_file_path ]; then
+  echo "File '$package_file_name' does not exist at '$file_location', creating it..."
+  touch $package_file_path
+  cat > $package_file_path << EOF
 {
   "name": "@generated/prisma",
   "main": "client.ts",
-  "types": "client.ts",
+  "types": "client.ts"
 }
 EOF
 else
-  echo "File '$file_name' already exists at '$file_location', skipping creation."
+  echo "File '$package_file_name' already exists at '$file_location', skipping creation."
 fi
 
+index_file_name="index.ts"
+index_file_path=$file_location/$index_file_name
+
+echo "Checking if file '$index_file_name' exists at '$file_location'..."
+if [ ! -f $index_file_path ]; then
+  echo "File '$index_file_name' does not exist at '$file_location', creating it..."
+  touch $index_file_path
+  cat > $index_file_path << EOF
+export * from './client';
+EOF
+else
+  echo "File '$index_file_name' already exists at '$file_location', skipping creation."
+fi
